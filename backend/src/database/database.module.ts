119fdf548ade9465e36ee2ea0af302772ca86997
Ayesha Khan <ayeshanadeem9085@gmail.com>
2023-11-07 14:42:06 +0500

    removed extra code

--- backend/src/database/database.module.ts
+++ backend/src/database/database.module.ts
@@ -3,15 +3,6 @@ import { ConfigService } from "@nestjs/config";
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { connectionSource } from './data-source';
 
-interface DatabaseConfig {
-    host: string;
-    port: number;
-    username: string;
-    password: string;
-    type: 'mysql';
-    database: string
-}
-
 @Module({
     imports: [
         TypeOrmModule.forRootAsync({
